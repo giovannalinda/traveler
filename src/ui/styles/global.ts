ff70d7f35bfab36bb6583ac70265b42ae1087fe4
Giovanna Linda <euGiovannaSouza@gmail.com>
2021-09-28 22:45:10 -0300

    style: definition of color palette and typography

---
+++ src/ui/styles/global.ts
@@ -0,0 +1,29 @@
+import { createGlobalStyle } from 'styled-components'
+import { theme } from './theme'
+
+export const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    outline: 0;
+    box-sizing: border-box;
+  }
+
+  body {
+    background: ${theme.colors.white[100]};
+    font-family: ${theme.fonts.Barlow.sizes.medium};
+  }
+
+  html {
+    font-size: 62.5%;
+  }
+  button {
+    cursor: pointer;
+  }
+  a {
+    text-decoration: none;
+  }
+  ul {
+    list-style: none;
+  }
+`
---
+++ src/ui/styles/theme.ts
@@ -0,0 +1,34 @@
+export const theme = {
+  fonts: {
+    Barlow: {
+      sizes: {
+        normal: 500,
+        medium: 600,
+        bold: 700,
+      },
+    },
+    Heebo: {
+      normal: 400,
+      medium: 500,
+    },
+    Roboto: 400,
+  },
+  colors: {
+    orange: '#F25D27',
+    green: '#51B853',
+    white: {
+      50: '#FFFFFF',
+      100: '#F5F8FA',
+    },
+    blue: {
+      50: '#DDE9F0',
+      400: '#115D8C',
+      700: '#123952',
+    },
+    gray: {
+      50: '#DCE2E6',
+      300: '#A0ACB3',
+      400: '#617480',
+    },
+  },
+}
